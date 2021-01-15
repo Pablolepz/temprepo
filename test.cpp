7ee8dc2f02da0fb978b80547024fa8a74432dc32
Pablolepz <plepe00@gmail.com>
2021-01-14 20:55:35 -0800

    Added add function

--- test.cpp
+++ test.cpp
@@ -3,10 +3,32 @@
 
 using namespace std;
 
+int calcAdd(int a, int b)
+{
+    return a + b;
+}
+
 int main(){
 
     std::cout << "test" << std::endl;
     int x = 0;
 
+
+    //unit testing
+
+    //calcAdd()
+    int result = calcAdd(4, 5);
+
+    if (result == 9)
+    {
+        cout << "calcAdd pass" << endl;
+        return 0;
+    }
+    else
+    {
+        cout << "calcAdd fail" << endl;
+        return 1;
+    }
+    
     return 0;
 }
