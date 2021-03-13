f8c3dd672d1d6a1faeb2ccf5267756271067a236
Pablolepz <plepe00@gmail.com>
2021-03-13 13:27:54 -0800

    Updated code for snake case

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
 
     std::cout << "test" << std::endl;
-    std::cout << "newLine" << std::endl;
+    std::cout << "new_line" << std::endl;
     int x = 0;
 
     return 0;
